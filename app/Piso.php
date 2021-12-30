95074544fa36b93a49d665568f92280eea02e354
Avimael Gervacio Requena <urbanolimahuacho@gmail.com>
2021-12-30 11:30:47 -0500

    Se crea los modelos Piso y TipoHabitacion

---
+++ app/Piso.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Piso extends Model
+{
+     //protected $Piso = 'pisos';  ya no definimos por que seguimos las reglas de laravel
+    //protected $primaryKey = 'id';
+    protected $fillable = ['nombre','descripcion'];
+}
---
+++ app/Tipo_habitacion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tipo_habitacion extends Model
+{
+    protected $fillable =['nombre','descripcion','condicion'];
+}
