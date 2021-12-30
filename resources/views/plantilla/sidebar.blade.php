db008e65af9af04d8d6adebf947163b2a9c80f14
Avimael Gervacio Requena <urbanolimahuacho@gmail.com>
2021-12-30 11:08:14 -0500

    Se completa las migraciones de la tabla pisos y tipo de habitaciones

---
+++ resources/views/plantilla/sidebar.blade.php
@@ -0,0 +1,93 @@
+<div class="sidebar">
+            <nav class="sidebar-nav">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a @click="menu=0" class="nav-link active" href="#"><i class="icon-speedometer"></i> Escritorio</a>
+                    </li>
+                    <li class="nav-title">
+                        Mantenimiento
+                    </li>
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-bag"></i>Gestion</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=1" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-bag"></i> Recepcion</a>
+                            </li>
+                            <li @click="menu=2" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-bag"></i> Salida</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i>Mantenimiento</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=3" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-basket-loaded"></i>Habitaciones</a>
+                            </li>
+                            <li @click="menu=4" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-notebook"></i>Tipo Habitacion</a>
+                            </li>
+                            <li @click="menu=5" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-notebook"></i>Pisos</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-wallet"></i> Almacén</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=6" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-wallet"></i> Categórias</a>
+                            </li>
+                            <li @click="menu=7" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-notebook"></i>Productos</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i>Tienda</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=8" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-basket-loaded"></i> Ventas</a>
+                            </li>
+                            <li @click="menu=9" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-notebook"></i> Clientes</a>
+                            </li>
+                        </ul>
+                    </li>
+                   
+                
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-people"></i> Acceso</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=10" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-user"></i> Usuarios</a>
+                            </li>
+                            <li @click="menu=11" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-user-following"></i> Roles</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-pie-chart"></i> Reportes</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=12" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-chart"></i> Reporte Recepcion</a>
+                            </li>
+                            <li @click="menu=13" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-chart"></i> Reporte Ventas</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li @click="menu=14" class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-book-open"></i> Ayuda <span class="badge badge-danger">PDF</span></a>
+                    </li>
+                    <li @click="menu=15" class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-info"></i> Acerca de...<span class="badge badge-info">IT</span></a>
+                    </li>
+                </ul>
+            </nav>
+            <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+        </div>
